35955b0fd7766d2aeb598cd9ed5f65eee06becdd
valenfei05 <147516780+valenfei05@users.noreply.github.com>
2023-11-14 15:53:31 -0500

    Update parcial_programación.py

---
+++ parcial_programación.py
@@ -0,0 +1,7 @@
+def get_binary(text):
+    binary_result = ""
+    for i in text:
+        ascii_value = ord(i)  
+        binary_value = bin(ascii_value)[2:]  
+        binary_result += binary_value.zfill(8)  
+    return binary_result
