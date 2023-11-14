06f1c1397040c31a1fc7c967c636b070fb051443
valenfei05 <147516780+valenfei05@users.noreply.github.com>
2023-11-14 16:35:35 -0500

    Update parcial_programación.py

---
+++ parcial_programación.py
@@ -0,0 +1,18 @@
+from operations import get_ascii, get_binary, get_resuts
+import sys
+
+menu=int(input('Menu\n=====\n1. Character\n2. Word'))
+
+if (menu==1):
+    char=input('Enter a character:')
+    word=char
+elif (menu==2):
+    word=input('Enter a word:')
+else:
+    sys.exit()
+
+print('Results\n=========')
+
+results= get_results(word)
+print('Total: {0}.format(results)')
+          
