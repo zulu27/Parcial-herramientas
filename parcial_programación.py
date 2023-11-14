5d82f42ad9fb0d932018de951de46604af7fd38f
bellasiv <147517101+bellasiv@users.noreply.github.com>
2023-11-14 17:06:24 -0500

    Update parcial_programación.py

---
+++ parcial_programación.py
@@ -0,0 +1,7 @@
+def get_results(palabra):
+    resultado_concatenado = 'Results\n=======\n'
+    for char in palabra:
+        valor_ascii = get_ascii(char)
+        binario = get_binary(char)
+        resultado_concatenado += f'ASCII character value of "{char}" is {valor_ascii}. Binary representation of "{char}" in a Byte is {binario}\n'
+    return resultado_concatenado
