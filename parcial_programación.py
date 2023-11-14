f0b9931fa7c9179d12f08b9e1b9e42b2466604cd
zulu27 <143005593+zulu27@users.noreply.github.com>
2023-11-14 17:10:13 -0500

    Update parcial_programación.py

--- parcial_programación.py
+++ parcial_programación.py
@@ -1,6 +1,6 @@
-def get_binary(word):
+def get_binary(char):
     binary_result = ""
-    for i in word:
+    for i in char:
         ascii_value = ord(i)  
         binary_value = bin(ascii_value)[2:]  
         binary_result += binary_value.zfill(8)  
