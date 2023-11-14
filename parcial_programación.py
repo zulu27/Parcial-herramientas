80b37b83d607d397756c00b872fe06388e2d7489
zulu27 <143005593+zulu27@users.noreply.github.com>
2023-11-14 17:29:38 -0500

    Update parcial_programación.py

--- parcial_programación.py
+++ parcial_programación.py
@@ -1,20 +1,11 @@
-
-def get_ascii(char):
-    valor = ord(char)
-         return valor
-    
-def get_binary(char):
-    binary_result = ""
-    for i in char:
-        ascii_value = ord(i)  
-        binary_value = bin(ascii_value)[2:]  
-        binary_result += binary_value.zfill(8)  
-    return binary_result
-  
- def get_results(word):
-    resultado_concatenado = 'Results\n=======\n'
+ 
+def get_results(word):
+    lista_codigos = ""
     for char in word:
-        valor_ascii = get_ascii(char)
+        
         binario = get_binary(char)
-        resultado_concatenado += f'ASCII character value of "{char}" is {valor_ascii}. Binary representation of "{char}" in a Byte is {binario}\n'
-    return resultado_concatenado
+        codigo = get_ascii(char)
+        print ("Ascii character value for ", char, " is" ,codigo,". Binary representation of ",char," in a Byte is",binario)
+        lista_codigos += binario
+        lista_codigos += " "
+    print("Total:",lista_codigos)
